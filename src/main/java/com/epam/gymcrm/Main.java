17b302faaf4bce8da99fbe4bc48683d815c2d929
Ihor Prokhorchuk <ihor_prokhorchuk@epam.com>
2026-04-18 14:26:19 +0300

    Feat: Add property source configuration and placeholder support in `Main` class

--- src/main/java/com/epam/gymcrm/Main.java
+++ src/main/java/com/epam/gymcrm/Main.java
@@ -1,13 +1,18 @@
 package com.epam.gymcrm;
 
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.*;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 
 @ComponentScan("com.epam.gymcrm")
 @Configuration
+@PropertySource("classpath:application.properties")
 public class Main {
+
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer propertyConfigurer() {
+        return new PropertySourcesPlaceholderConfigurer();
+    }
     public static void main(String[] args) {
         ApplicationContext context =
                 new AnnotationConfigApplicationContext(Main.class);

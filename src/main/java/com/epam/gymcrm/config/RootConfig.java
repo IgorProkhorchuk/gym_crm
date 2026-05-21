d451579774f75108c60497391a0ad29b87e3cc1d
Ihor Prokhorchuk <ihor_prokhorchuk@epam.com>
2026-05-21 22:38:27 +0300

    Feat: Add Spring Web MVC configuration, Tomcat deployment setup, and healthcheck REST controller

---
+++ src/main/java/com/epam/gymcrm/config/RootConfig.java
@@ -0,0 +1,23 @@
+package com.epam.gymcrm.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+
+@Configuration
+@ComponentScan(basePackages = "com.epam.gymcrm",
+excludeFilters = @ComponentScan.Filter(
+    type = FilterType.REGEX,
+    pattern = "com\\.epam\\.gymcrm\\.web\\..*"
+))
+@PropertySource("classpath:application.properties")
+@PropertySource(value = "file:${user.dir}/.env", ignoreResourceNotFound = true)
+public class RootConfig {
+  @Bean
+  public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
+    return new PropertySourcesPlaceholderConfigurer();
+  }
+}
---
+++ src/main/java/com/epam/gymcrm/web/controller/HelloController.java
@@ -0,0 +1,14 @@
+package com.epam.gymcrm.web.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v1")
+public class HelloController {
+  @GetMapping("/hello")
+    public String helloWorld() {
+      return "<h1>Hello World!</h1>";
+    }
+}

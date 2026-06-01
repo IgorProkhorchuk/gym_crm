1b37c9b960bf7019ed69dfa839b5d14e2398ca60
Ihor Prokhorchuk <ihor_prokhorchuk@epam.com>
2026-06-02 00:19:42 +0300

    Refactor: Replace manual Spring configuration with Spring Boot auto-configuration and add environment profiles

---
+++ src/main/resources/data-h2.sql
@@ -0,0 +1,7 @@
+merge into training_types (training_type_name) key (training_type_name) values ('Fitness');
+merge into training_types (training_type_name) key (training_type_name) values ('Yoga');
+merge into training_types (training_type_name) key (training_type_name) values ('Zumba');
+merge into training_types (training_type_name) key (training_type_name) values ('Stretching');
+merge into training_types (training_type_name) key (training_type_name) values ('Resistance');
+merge into training_types (training_type_name) key (training_type_name) values ('Cardio');
+merge into training_types (training_type_name) key (training_type_name) values ('Boxing');

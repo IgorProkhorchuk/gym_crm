35930c1cbb1f13b7368dd7743539875d289f63eb
Ihor Prokhorchuk <ihor_prokhorchuk@epam.com>
2026-06-01 23:46:08 +0300

    Refactor: Remove WebAppInitializer and simplify WebConfig by configuring the servlet path in application properties

--- src/main/java/com/epam/gymcrm/config/WebConfig.java
+++ src/main/java/com/epam/gymcrm/config/WebConfig.java
@@ -2,17 +2,11 @@ package com.epam.gymcrm.config;
 
 import com.epam.gymcrm.web.logging.RestLoggingInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@EnableWebMvc
-@Import(OpenApiDocumentationConfig.class)
-@ComponentScan(basePackages = "com.epam.gymcrm.web")
 public class WebConfig implements WebMvcConfigurer {
 
   private final RestLoggingInterceptor restLoggingInterceptor;

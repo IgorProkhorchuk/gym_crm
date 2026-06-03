a8fd74f81a8e81fb1b79daa2d25a47880f4e0b93
Ihor Prokhorchuk <ihor_prokhorchuk@epam.com>
2026-06-03 22:56:49 +0300

    Refactor: Use Lombok's @RequiredArgsConstructor in WebConfig to simplify constructor injection

--- src/main/java/com/epam/gymcrm/config/WebConfig.java
+++ src/main/java/com/epam/gymcrm/config/WebConfig.java
@@ -1,20 +1,16 @@
 package com.epam.gymcrm.config;
 
 import com.epam.gymcrm.web.logging.RestLoggingInterceptor;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
 
   private final RestLoggingInterceptor restLoggingInterceptor;
-
-  @Autowired
-  public WebConfig(RestLoggingInterceptor restLoggingInterceptor) {
-    this.restLoggingInterceptor = restLoggingInterceptor;
-  }
 
   @Override
   public void addInterceptors(InterceptorRegistry registry) {
